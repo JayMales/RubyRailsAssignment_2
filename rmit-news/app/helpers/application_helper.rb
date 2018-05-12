80fe8004449f2fb5c152bb536ecc519468c841ff
Anthony <chizzele.oce@gmail.com>
2018-05-12 13:47:02 +1000

    Comments styled
    Everything is now styled and looking pretty good.

--- rmit-news/app/helpers/application_helper.rb
+++ rmit-news/app/helpers/application_helper.rb
@@ -1,18 +1,22 @@
 module ApplicationHelper
 
-  def prettyDate(dateNow, dateCreated)
-       minutes_since_post = (dateNow - dateCreated).round(0) / 60
+  def prettyDate(dateCreated)
+       minutes_since_post = (DateTime.now.utc - dateCreated).round(0) / 60
        if minutes_since_post > 59
          hours = minutes_since_post / 60
          if hours > 24
            days = (hours / 24).round(0)
-           if days > 2
+           if days = 1
              days = "a day ago"
            else
            days = "#{days} days ago"
          end
          else
-           hours =  "about #{hours} hours ago"
+           if hours = 1
+             hours =  "about an hour ago"
+           else
+             hours =  "about #{hours} hours ago"
+           end
          end
        else
          if minutes_since_post == 0
@@ -24,5 +28,5 @@ module ApplicationHelper
          end
        end
      end
-     
+
 end
