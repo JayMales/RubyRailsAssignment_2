826af2ae6c303741e4866583f86420ebe4b84bb9
s3486715 <jaymales94@gmail.com>
2018-05-14 21:09:08 +1000

    Added create to the api

--- rmit-news/app/controllers/apiv0_controller.rb
+++ rmit-news/app/controllers/apiv0_controller.rb
@@ -14,6 +14,31 @@ class Apiv0Controller < ApplicationController
   end
 
   def create
+    begin
+      item = Item.new(item_params)
+      item.user_id = Item.where(username: params[:username]).id
+      if params[:type] == "Comment"
+        if item.post_id.nil?
+          raise "post id required"
+        end
+        item.type = "Comment"
+        item.save
+      elsif params[:type] == "News"
+        item.type = "Post"
+        item.save
+      end
 
+    rescue => ex
+      render plain: "{Error: 400 bad request}", :status => :bad_request
+    end
   end
+
+  private
+    def item_params
+      if params[:type] == "Comment"
+        params.permit(:type, :text, :post_id,:username)
+      elsif params[:type] == "News"
+        params.permit(:type, :text, :title, :url,:username)
+      end
+    end
 end
